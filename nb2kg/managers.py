66c36d5bfd177e73e4bffea4c061e693ad0ab794
tarunaz <tmehra@redhat.com>
2018-03-13 16:07:10 -0700

    Update managers.py

--- nb2kg/managers.py
+++ nb2kg/managers.py
@@ -49,9 +49,8 @@ def load_connection_args(**kwargs):
     kwargs['connect_timeout'] = kwargs.get('connect_timeout', KG_CONNECT_TIMEOUT)
     kwargs['request_timeout'] = kwargs.get('request_timeout', KG_REQUEST_TIMEOUT)
     kwargs['headers'] = kwargs.get('headers', KG_HEADERS)
-    kwargs['validate_cert'] = kwargs.get('validate_cert', VALIDATE_KG_CERT)
-    kwargs['auth_username'] = kwargs.get('auth_username', KG_HTTP_USER)
-    kwargs['auth_password'] = kwargs.get('auth_password', KG_HTTP_PASS)
+    kwargs['validate_cert'] = None #kwargs.get('auth_username', KG_HTTP_USER)
+    kwargs['auth_password'] = None #kwargs.get('auth_password', KG_HTTP_PASS)
     return kwargs
 
 @gen.coroutine
